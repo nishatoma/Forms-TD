bc1d436773334be56b04b52efcff41e219ba4d31
nishatoma <nishatoma@gmail.com>
2019-02-19 22:27:35 -0800

    update

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  @ViewChild('f') form: NgForm;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   console.log("email: " + form.value.email);
+  //   console.log("username: " + form.value.username);
+  // }
+
+  // Another way to access via the viewchild
+  onSubmit() {
+    console.log(this.form);
   }
 }
