0a43a6eb1eb59fa089620e31e7eedd9a0175f0a1
nishatoma <nishatoma@gmail.com>
2019-02-20 16:26:50 -0800

    TD forms update

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,41 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+  
 
   @ViewChild('f') form: NgForm;
   defaultQuestion = 'pet';
   answer: string = '';
+  genders = ['male', 'female', 'other'];
+
+  user1 = {
+    username: '',
+    email: '',
+    secret: '',
+    gender: 'sadas',
+    answer: '',
+  };
+    
+  
+
+  submitted: boolean = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   }, 
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    // =============================
+    // Set value is for the whole form while patch
+    // value is for one aspect of it.
+    this.form.form.patchValue({userData: {username:suggestedName}});  
+  }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
@@ -23,6 +51,14 @@ export class AppComponent {
 
   // Another way to access via the viewchild
   onSubmit() {
-    console.log(this.form);
+    this.submitted = true;
+    console.log(this.user1.username);
+    this.user1.username = this.form.value.userData.username;
+    console.log(this.user1.username);
+    this.user1.email = this.form.value.userData.email;
+    this.user1.secret = this.form.value.secret;
+    this.user1.answer = this.form.value.questionAnswer;
+    this.user1.gender = this.form.value.gender;
+    this.form.reset();
   }
 }
